69a95ed6715fc4183ed5b4ead438ecce057530e2
Klawus <32956571+Klawus@users.noreply.github.com>
2017-11-01 23:24:17 +0100

    Add files via upload
    Adding a rate with +,-

--- srednia_mobireg.js
+++ srednia_mobireg.js
@@ -5,12 +5,19 @@ let last_result = 0;
 for (let j = 2; j < allDivTr.length;j++){
 	const spans = allDivTr[j].getElementsByTagName("SPAN");
 
-	const toInt = [];
+	const toFloat = [];
 	for(i=0;i<spans.length;i++){
-		toInt.push(parseInt(spans[i].textContent));
+		toFloat.push(parseFloat(spans[i].textContent));
+
+		if(spans[i].textContent == toFloat[i] + "+"){
+			toFloat[i] += 0.5;
+		}
+		if(spans[i].textContent == toFloat[i] + "-"){
+			toFloat[i] -= 0.1;
+		}
 	}
 
-	const rate = toInt.filter(x => x > 0);
+	const rate = toFloat.filter(x => x > 0);
 
 	let result = 0;
 	for(i=0;i<rate.length;i++){
@@ -28,7 +35,7 @@ for (let j = 2; j < allDivTr.length;j++){
 	}
 	else {
 		k[0].innerHTML = result;
-		results.push(parseInt(result));
+		results.push(parseFloat(result));
 		if(result>1.75){
 			k[0].style.border = "solid 2px #006400";
 			k[0].style.color = "black";
@@ -47,3 +54,5 @@ last_result = last_result.toFixed(2);
 
 const th = allDivTr[0].getElementsByTagName("TH");
 th[1].innerHTML = "Ocena koncowa to: " + last_result;
+
+
