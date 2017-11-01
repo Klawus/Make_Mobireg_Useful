c633c150d05836a5944f88ca68f745395a15cd98
Klawus <32956571+Klawus@users.noreply.github.com>
2017-11-01 14:55:42 +0100

    Add files via upload
    It work

---
+++ srednia_mobireg.js
@@ -0,0 +1,49 @@
+const allDivTr = document.getElementById("markPlace").getElementsByTagName("TR");
+const results = [];
+let last_result = 0;
+
+for (let j = 2; j < allDivTr.length;j++){
+	const spans = allDivTr[j].getElementsByTagName("SPAN");
+
+	const toInt = [];
+	for(i=0;i<spans.length;i++){
+		toInt.push(parseInt(spans[i].textContent));
+	}
+
+	const rate = toInt.filter(x => x > 0);
+
+	let result = 0;
+	for(i=0;i<rate.length;i++){
+		result += rate[i];
+	}
+	result /= rate.length;
+
+	result = result.toFixed(2);
+
+	let k = allDivTr[j].getElementsByClassName("ocena_koncowa");
+
+	if(isNaN(result)){
+		k[0].innerHTML = "Brak danych";
+		k[0].style.color = "#FF3300";
+	}
+	else {
+		k[0].innerHTML = result;
+		results.push(parseInt(result));
+		if(result>1.75){
+			k[0].style.border = "solid 2px #006400";
+			k[0].style.color = "black";
+		}
+		else{
+			k[0].style.color = "#FF3300";
+		}
+	}
+}
+
+for(i=0; i<results.length;i++){
+	last_result += results[i];
+}
+last_result /= results.length;
+last_result = last_result.toFixed(2);
+
+const th = allDivTr[0].getElementsByTagName("TH");
+th[1].innerHTML = "Ocena koncowa to: " + last_result;
